f673961d42a4ff931cad02de20af4594aa424cde
Juan Cruz <juansaldano01@gmail.com>
2024-11-26 20:45:00 +0000

    Responsivness added

--- pages/B_wine.py
+++ pages/B_wine.py
@@ -1,6 +1,5 @@
 import reflex as rx
 from links import Links
-from styles.styles import format_page
 
 
 @rx.page(
@@ -9,10 +8,9 @@ from styles.styles import format_page
 def wine_page() -> rx.Component:
     return rx.box(
         rx.container(
-            rx.vstack(
-                rx.heading("Streamlit Dashboard", size="8"),
-                rx.markdown(
-                    """
+            rx.heading("Streamlit Dashboard", size="8", align="center", width="100%", ),
+            rx.markdown(
+                """
                     This project comes from one of my college subjects. The aim was to build a visualization or dashboard where I could summarize the dataset and present it in an easy-to-understand format, making it easier to analyze and interpret the data. In this case, the dataset contained wine reviews, which included information about wines reviewed by sommeliers. Among the columns, you can find data on wine geography, sommelier information, wine descriptions, prices, and ratings.
 
                     First, I had to clean and process the data. I organized it by continents, removed duplicates, and imputed missing numeric values. I also performed sentiment analysis on the wine descriptions to classify them as positive, negative, or neutral, among other things.
@@ -20,8 +18,10 @@ def wine_page() -> rx.Component:
                     - Geographic Analysis: I analyzed the reviews based on geographic data, such as continent of production, and examined metrics like average price by country.
                     - Winery Analysis: I focused on basic data about the producers, such as total production and prices.
                     - Price Analysis: I wanted to verify the following hypothesis: "Older wines are better, and therefore should be more expensive." In this dataset, however, this assumption was not accurate. I first conducted a correlation analysis, which showed no relationship between wine age and price. Then, I created a graphic comparing price, age, and ratings, which further confirmed the lack of correlation
-                """
-                ),
+                """,
+                margin="1em",
+            ),
+            rx.center(
                 rx.hstack(
                     rx.text(rx.text.strong("Repository: ")),
                     rx.link(
@@ -30,15 +30,20 @@ def wine_page() -> rx.Component:
                         is_external=True,
                         padding_top="5px",
                     ),
-                    align="start",
+                    align="center",
+                    margin="1em",
                 ),
-                rx.text("# If you don't want to download the entire project, I’ve added a preview video."),
-                rx.video(
-                    url="/wine_record.mp4",
-                    width="800px",
-                    height="auto",
-                ),
-                **format_page
+            ),
+            rx.text(
+                "If you don't want to download the entire project, I’ve added a preview video.",
+                padding_top="15px",
+                margin="1em",
+            ),
+            rx.video(
+                url="/wine_record.mp4",
+                width="auto",
+                height="auto",
             ),
         ),
+        width="100%",
     )
--- pages/C_crud.py
+++ pages/C_crud.py
@@ -9,10 +9,14 @@ from styles.styles import format_page
 def crud_page() -> rx.Component:
     return rx.box(
         rx.container(
-            rx.vstack(
-                rx.heading("Crud Aplication", size="8"),
-                rx.markdown(
-                    """
+            rx.heading(
+                "Crud Aplication",
+                size="8",
+                align="center",
+                width="100%",
+            ),
+            rx.markdown(
+                """
                     This is a CRUD project, and it can be defined as follows: CRUD is an acronym for Create, Read, Update, and Delete. Each of these performs a different operation, but they all aim to track and manage data from a database. I developed this CRUD project using Python code and MySQL as the database. I used a library called SQLModel, which is similar to SQLAlchemy, but the key difference is its integration with Pydantic.
 
                     In my case, I chose this library because it is easier to integrate with FastAPI, as they share the same creator. This CRUD app is designed for an ice cream shop, allowing the user to insert data for products and weekly purchases. It is a very simple database that contains three tables:
@@ -21,14 +25,19 @@ def crud_page() -> rx.Component:
                     - Receipt: Contains the receipt data. I created this table to minimize redundancy in the second table. Without it, if a weekly purchase includes 15 different ice cream flavors on the same receipt, the receipt data would be repeated 15 times.
                     
                     My Python code is organized into classes. Initially, I planned to create a class for each table, but then I realized that this would result in repetitive code (each class would have the same CRUD operations). So, I changed my approach. I developed a class for each CRUD operation, so each class is responsible for just one CRUD action. To test the application, I included tests using pytest, which test each function.
-                    """
-                ),
-                rx.text("Entity Relationship Diagram:"),
-                rx.image(
-                    "/ERD.png",
-                    width="800px",
-                    height="auto",
-                ),
+                    """,
+                margin="1em",
+            ),
+            rx.text(
+                rx.text.strong("Entity Relationship Diagram:"),
+                margin="1em",
+            ),
+            rx.image(
+                "/ERD.png",
+                width="autox",
+                height="auto",
+            ),
+            rx.center(
                 rx.hstack(
                     rx.text(rx.text.strong("Repository: ")),
                     rx.link(
@@ -37,9 +46,9 @@ def crud_page() -> rx.Component:
                         is_external=True,
                         padding_top="5px",
                     ),
-                    align="start",
+                    align="center",
+                    margin="1em",
                 ),
-                **format_page
             ),
         ),
     )
